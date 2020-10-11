6ce1cb7368ec1c52318a13f15e39fad8be6a977d
Magdalena Lisicka <magdalisicka@yahoo.co.uk>
2020-10-11 19:10:32 +0100

    index js and projects

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,56 @@
 import '../scss/main.scss';
 
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+fetch("https://api.github.com/users/Magdalena-Lisicka/repos?sort=created&direction=asc")
+  .then((res) => res.json())
+  .then((res) => {
+    const container = document.querySelector('.projects-grid--js');
+    for (let repo of res) {
+        const {description, homepage, html_url, name} = repo;
+
+        const template = ` <article class="project"> 
+          
+        <div class="project__window">
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+
+        </div>
+        <div class="project__content">
+          <!--<img src="../assets/img/github.svg" alt="">  githab's icon, or a href link -->
+        <h3 class="project__grid project__title">
+          <span class="project__label">project:</span> 
+          <span>${name}</span>
+        </h3>
+        <p class="project__grid">
+          <span class="project__grid">description:</span>
+          <span>${description}</span>
+        </p>
+        <p class="project__grid">
+          <span class="project__grid project__label">demo:</span>
+          <span>
+            &lt;<a 
+            class="project__link" 
+            href="${homepage}" 
+            title="${name}- demo">see_here</a>&gt;
+          </span>
+        </p>
+        <p class="project__grid">
+          <span class="project__grid project__label">github:</span>
+          <span>
+            &lt;<a 
+            class="project__link" 
+            href="${html_url}" 
+            title="${name} - code">source_code</a>&gt;
+          </span>
+        </p>
+      </div>
+      </article>`;
+      if (description) {
+        container.innerHTML += template;
+      }
+    }
+  })
+  .catch((e) => console.log(e));
